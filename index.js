4800405031ce85fbcf9ddb39aa7f8acaf0d63fea
mnl187 <mnl187@gmail.com>
2023-03-22 07:24:37 +0100

    dalsza konfiguracja pliku startowego serwera: dodanie połacenia z bazą, dodanie cors do obsługi żadań API

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-
 const express = require('express');
-
+const cors = require('cors')
+const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost/brewhouse', {})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: {PORT}`)))
+    .catch((error) => console.log(error.message))
 
