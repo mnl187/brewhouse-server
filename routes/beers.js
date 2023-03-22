9d3e13818fae5cf26e240ad2218e5517db87d98b
mnl187 <mnl187@gmail.com>
2023-03-22 22:46:53 +0100

    dodanie obśługi logowania w konsoli danych wysyłanych do API z Postmana

--- routes/beers.js
+++ routes/beers.js
@@ -9,8 +9,13 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
+        console.log('Received data:', req.body); // Dodaje logowanie otrzymanych danych
+
         const newBeer = new Beer(req.body);
         const savedBeer = await newBeer.save();
+
+        console.log('Saved beer:', savedBeer); // Dodaje logowanie zapisanego piwa
+
         res.json(savedBeer);
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -20,6 +25,11 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     const updatedBeer = await Beer.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(updatedBeer);
+});
+
+router.delete('/:id', async (req, res) => {
+    await Beer.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Beer deleted' });
 });
 
 module.exports = router;
