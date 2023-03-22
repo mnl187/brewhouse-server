69d8e571ccf529f1b4b62874ddf083dd5eb7c5d6
mnl187 <mnl187@gmail.com>
2023-03-22 19:13:23 +0100

    dalsza konfiguracja db.js, dodanie opcji połaczenia z db, zmiana implementacji funkcji connectDB oraz dodanie jej wywołania w index.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connectDB = async () => {
+async function connectDB() {
     try {
-        console.log('MongoDB connected')
-    } catch {
-
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
     }
 }
 
-module.exports = {
-    connectDB,
-}
+module.exports = connectDB;
