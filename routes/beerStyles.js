24efd0a8d7cb51c478cebe808ab35eae5aff764b
mnl187 <mnl187@gmail.com>
2023-04-04 19:51:55 +0200

    dodanie nowego endpoint do wyswietlania pojedynczego stylu piwa

--- routes/beerStyles.js
+++ routes/beerStyles.js
@@ -7,4 +7,16 @@ router.get('/', (req, res) => {
     res.json(beerStyles);
 });
 
+// Endpoint GET do pobierania wszystkich 1 stylu piwa
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const beerStyle = beerStyles.find((style) => style.number === id);
+
+    if (beerStyle) {
+        res.json(beerStyle);
+    } else {
+        res.status(404).json({ message: "Beer style not found" });
+    }
+});
+
 module.exports = router;
