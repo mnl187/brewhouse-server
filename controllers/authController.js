cbd0a3afffdf8b6429bda79eb5ced57e30a697ee
mnl187 <mnl187@gmail.com>
2023-04-05 20:45:18 +0200

    dodanie obłsugi metod http

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,12 +4,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const register = async (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
 
     // Sprawdzenie czy użytkownik istnieje
-    const existingUser = await User.findOne({ username });
+    const existingUser = await User.findOne({username});
     if (existingUser) {
-        return res.status(400).json({ message: 'Nazwa użytkownika jest już zajęta' });
+        return res.status(400).json({message: 'Nazwa użytkownika jest już zajęta'});
     }
 
     // Szyfrowanie hasła
@@ -17,8 +17,15 @@ const register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // Utworzenie nowego użytkownika
-    const newUser = new User({ username, password: hashedPassword});
+    const newUser = new User({username, password: hashedPassword});
 
+    // obsługa metod http
+    try {
+        await newUser.save();
+        res.status(201).json({message: 'Użytkownik zarejestrowany pomyślnie'});
+    } catch (error) {
+        res.status(500).json({message: 'Błąd podczas rejestracji użytkownika', error});
+    }
 };
 
 const login = (req, res, next) => {
